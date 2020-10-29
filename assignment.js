7b20e12ba40f7a6025cbff7d9c626c0047f781b4
C-Warden <charles.warden@amalitech.org>
2020-10-29 09:00:56 +0000

    complete assignment 5

---
+++ assignment.js
@@ -0,0 +1,18 @@
+// solve task 1
+const lia = document.querySelector('li');
+const lib = document.getElementById('task-1');
+
+lia.style.backgroundColor = 'black';
+lib.style.textColor = 'white';
+
+//solve task 2
+const title = document.querySelector('title');
+const head = document.head;
+title2 = head.querySelector('title');
+
+title.textContent = 'Assignment Solved!'
+    //title2.textContent = 'Assignment Solved!';
+
+//solve task 3
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment Solved!';
